b296303a7bed64b11c1b6375614639046f6f19d7
丁松杰 <377973147@qq.com>
2023-10-24 08:42:11 +0800

    删除多余的 代码

--- website/file_help.go
+++ website/file_help.go
@@ -84,7 +84,6 @@ func main() {
 	quit := make(chan os.Signal)
 	signal.Notify(quit, os.Interrupt)
 	<-quit
-	Initialize.DestoryConverters()
 	logger.Info("Shutdown Server ...")
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
